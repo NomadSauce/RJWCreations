37c18e775f12af6e65e8cd887caec6041f50743f
mgarsteck <g-photography@hotmail.com>
2023-11-19 14:39:54 -0500

    cart-items design

--- src/lib/CartItems.svelte
+++ src/lib/CartItems.svelte
@@ -1,22 +1,27 @@
 <script>
     export let data
+
+    function selectItem(item) {
+        console.log('Cart Item:', item)
+    }
 </script>
 
 <div class="d-inline-flex flex-column align-items-end align-content-stretch">
     {#each data as item }
-        <div class="row align-self-right align-items-center cart-item">
-            <h6 class=" text-white-50">{item.size} {item.blank} {item.stone} ${item.price}</h6>
+        <div class="row align-self-right align-items-center cart-item" on:click={() => selectItem(item)}>
+            <h6 class=" text-white-50 align-middle">{item.size} {item.blank} {item.stone} ${item.price}</h6>
         </div>
     {/each}
 </div>
 
 <style>
     .cart-item {
-        border: 1px solid rgb(51, 0, 170);
-        border-radius: 2rem;
-        background-color: rgb(41, 41, 41);
-        margin: 5px;
+        background: linear-gradient(rgb(46, 46, 46), rgb(46, 46, 46)) padding-box,
+              linear-gradient(to right, darkblue, darkorchid) border-box;
+        border-radius: 50em;
+        border: 2px solid transparent;
         padding: 5px;
+        margin: 5px;
 
     }
 </style>
